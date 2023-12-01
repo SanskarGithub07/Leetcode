77a122d1bf3cd2a941ed8c8588a3e64381da943c
SanskarGithub07 <117458101+SanskarGithub07@users.noreply.github.com>
2023-12-01 22:20:56 +0530

    update

--- 1662_Check_If_Two_String_Arrays_Are_Equivalent.cpp
+++ 1662_Check_If_Two_String_Arrays_Are_Equivalent.cpp
@@ -7,22 +7,14 @@ public:
         string firstString = "";
         string secondString = "";
 
-        for(int i = 0 ; i < word1.size() ; i++){
-            for(int j = 0 ; j < word1[i].length() ; j++){
-                firstString += word1[i][j];
-            }
+        for(int i = 0 ; i <  word1.size() ; i++){
+            firstString += word1[i];
         }
 
-        for(int i = 0 ; i < word2.size() ; i++){
-            for(int j = 0 ; j < word2[i].length() ; j++){
-                secondString += word2[i][j];
-            }
+        for(int i = 0 ; i <  word2.size() ; i++){
+            secondString += word2[i];
         }
 
-        if(firstString == secondString){
-            return true;
-        }
-
-        return false;
+        return firstString == secondString;
     }
 };
