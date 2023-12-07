a5f879fdd9d35030fa28aad295de8038a02a17ba
SanskarGithub07 <117458101+SanskarGithub07@users.noreply.github.com>
2023-12-07 21:15:55 +0530

    Daily Problem 7th December 2023.

---
+++ 1903_Largest_Odd_Number_In_String.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        string ans = "";
+        for(int i = num.length() - 1 ; i >= 0 ; i--){
+            if((num[i] - '0') % 2 != 0){
+                ans += num.substr(0, i + 1);
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
