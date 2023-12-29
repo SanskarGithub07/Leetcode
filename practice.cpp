d01494c45d69bdfcf41e15d56f6f6aa47eddaddd
SanskarGithub07 <117458101+SanskarGithub07@users.noreply.github.com>
2023-12-29 21:36:10 +0530

    Binary Tree Basic Problem.

--- practice.cpp
+++ practice.cpp
@@ -1,33 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> nextSmallerElement(vector<int> &arr){
+// Celebrity Problem
+int celebrity(vector<vector<int>> &M, int n){
     stack<int> stk;
-    stk.push(-1);
-    for(int i = arr.size() - 1 ; i >= 0 ; i--){
-        int val = stk.top();
-        if(arr[i] > val){
-            stk.push(arr[i]);
-            arr[i] = val;
+    for(int i = 0 ; i < n ; i++){
+        stk.push(i);
+    }
+
+    while(stk.size() != 1){
+        int A = stk.top();
+        stk.pop();
+        int B = stk.top();
+        stk.pop(); 
+
+        if(M[A][B]){
+            stk.push(B);
         }
-        else{
-            while(stk.top() >= arr[i]){
-                stk.pop();
+        else if(M[B][A]){
+            stk.push(A);
+        }
+    }
+
+    int potentialCelebrity = stk.top();
+    stk.pop();
+
+    bool rowCheck = true;
+    for(int i = 0 ; i < n ; i++){
+        if(M[potentialCelebrity][i] == 1){
+            rowCheck = false;
+            return -1;
+        }
+    }
+
+    bool columnCheck = true;
+    for(int row = 0 ; row < n ; row++){
+        if(potentialCelebrity != row){
+            if(M[row][potentialCelebrity] == 0){
+                columnCheck = false;
+                return -1;
             }
-            int newTopVal = stk.top();
-            stk.push(arr[i]);
-            arr[i] = newTopVal;
         }
     }
 
-    return arr;
-}
-int main(){
-    vector<int> arr = {2, 1, 4, 3};
-    arr = nextSmallerElement(arr);
-    for(int i = 0 ; i < arr.size() ; i++){
-        cout << arr[i] << " " ;
+    if(rowCheck && columnCheck){
+        return potentialCelebrity;
     }
 
-    return 0;
+    return -1;
+}
+
+int main(){
+    vector<vector<int>> M ={{0, 1, 0}, {0, 0, 0}, {0, 1, 0}};
+    int n = 3;
+
+    cout << celebrity(M, n) << endl; 
 }
