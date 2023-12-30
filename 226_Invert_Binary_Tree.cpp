7a1e7f74822511ac767cb3de8fddddb45703d1a0
SanskarGithub07 <117458101+SanskarGithub07@users.noreply.github.com>
2023-12-30 22:24:13 +0530

    Binary Tree Invert Problem.

---
+++ 226_Invert_Binary_Tree.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+   int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void helper(TreeNode * root){
+        if(root == NULL){
+            return;
+        }
+
+        helper(root->left);
+        helper(root->right);
+        if(root != NULL){
+            TreeNode * temp = root->left;
+            root->left = root->right;
+            root->right = temp;
+        }
+
+        return;
+    }
+    
+    TreeNode* invertTree(TreeNode* root) {
+        helper(root);
+        return root;
+    }
+};
