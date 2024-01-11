847d39d65d384823e00291831f36fe23fe0c705d
SanskarGithub07 <117458101+SanskarGithub07@users.noreply.github.com>
2024-01-11 12:57:06 +0530

    Medium heap problem.

--- practice.cpp
+++ practice.cpp
@@ -1,64 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// Time Complexity: O((m + n)log (m + n))
-// Space Complexity: O(m + n) 
+int getKthLargest(vector<int> &arr, int k){
+    priority_queue<int, vector<int>, greater<int>> mini;
 
-void heapify(vector<int> &mergedHeap, int n, int i){
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
+    for(int i = 0 ; i < arr.size() ; i++){
+        int sum = 0;
+        for(int j = i ; j < arr.size() ; j++){
+            sum += arr[j];
 
-    if(left < n && mergedHeap[left] > mergedHeap[largest]){
-        largest = left;
+            if(mini.size() < k){
+                mini.push(sum);
+            }
+            else{
+                if(sum > mini.top()){
+                    mini.pop();
+                    mini.push(sum);
+                }
+            }
+        }
     }
 
-    if(right < n && mergedHeap[right] > mergedHeap[largest]){
-        largest = right;
-    }
-
-    if(largest != i){
-        swap(mergedHeap[largest], mergedHeap[i]);
-        heapify(mergedHeap, n, largest);
-    }
-
-    return;
-}
-
-void buildMaxHeap(vector<int> &mergedHeap, int n){
-    for(int i = n / 2 - 1 ; i >= 0 ; i--){
-        heapify(mergedHeap, n, i);
-    }
-}
-
-vector<int> merge2BMH(vector<int> &MH1, vector<int> &MH2){
-    //Step1: Merge both the vectors into a single vector
-    vector<int> mergedHeap;
-
-    for(int i = 0 ; i < MH1.size() ; i++){
-        mergedHeap.push_back(MH1[i]);
-    }
-
-    for(int i = 0 ; i < MH2.size() ; i++){
-        mergedHeap.push_back(MH2[i]);
-    }
-
-    //Step2: Use buildMaxHeap and heapify functions to get the final mergedHeap.
-    int n = mergedHeap.size();
-    buildMaxHeap(mergedHeap, n);
-
-    return mergedHeap;
+    return mini.top();
 }
 
 int main(){
-    vector<int> MH1 = {10, 5, 6, 2};
-    vector<int> MH2 = {12, 7, 9};
+    vector<int> arr = {1, -2, 3, -4, 5};
+    int k = 2;
 
-    vector<int> ans = merge2BMH(MH1, MH2);
-
-    for(int i = 0 ; i < ans.size() ; i++){
-        cout << ans[i] << " ";
-    }cout << endl;
+    int sum = getKthLargest(arr, k);
+    cout << sum << endl;
 
     return 0;
 }
