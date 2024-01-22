3ab0059af54b5337f1e03e26306bb3fec7e154dc
SanskarGithub07 <117458101+SanskarGithub07@users.noreply.github.com>
2024-01-22 20:55:43 +0530

    Added sliding window approach to q3.

--- 3_Longest_Substring_Without_Repeating_Characters.cpp
+++ 3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -32,3 +32,37 @@ public:
         return ans;
     }
 };
+
+/*
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> repeated;
+        int ans = 0;
+        int localMax = 0;
+        int start = 0, end = 0;
+
+        while(end < s.length()){    
+            if(repeated.find(s[end]) != repeated.end()){
+               while(start < end && repeated.find(s[end]) != repeated.end()){
+                   repeated[s[start]]--;
+                   if(repeated[s[start]] == 0){
+                       repeated.erase(s[start]);
+                   }
+                   start++;
+               }
+               repeated[s[end]]++;
+               localMax =  end - start + 1;
+            }
+            else{
+                repeated[s[end]]++;
+                localMax++;
+            }
+
+            ans = max(localMax, ans);
+            end++;
+        }
+
+        return ans;
+    }
+};*/
