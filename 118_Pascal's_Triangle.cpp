26f27d298dbf8426770ed51a5d0c3e80f865fefd
SanskarGithub07 <117458101+SanskarGithub07@users.noreply.github.com>
2024-02-06 23:38:35 +0530

    Easy array loop problem.

---
+++ 118_Pascal's_Triangle.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        vector<int> pascalSum;
+
+        for(int i = 0 ; i < numRows; i++){
+            int twoSum = 0;
+            pascalSum.push_back(1);
+            for(int j = 0 ; j < i - 1 ; j++){
+                twoSum = ans[i - 1][j] + ans[i - 1][j + 1];
+                pascalSum.push_back(twoSum);
+                twoSum = 0;
+            }
+            if(i != 0){
+                pascalSum.push_back(1);
+            }
+            ans.push_back(pascalSum);
+            pascalSum.clear();
+        }
+
+        return ans;
+    }
+};
