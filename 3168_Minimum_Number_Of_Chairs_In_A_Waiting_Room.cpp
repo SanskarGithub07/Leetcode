4354946ce0b34c277c3b325ff29ef74f1f954e3e
SanskarGithub07 <117458101+SanskarGithub07@users.noreply.github.com>
2024-06-02 09:43:27 +0530

    update.

--- 3168_Minimum_Number_Of_Chairs_In_A_Waiting_Room.cpp
+++ 3168_Minimum_Number_Of_Chairs_In_A_Waiting_Room.cpp
@@ -3,12 +3,16 @@ using namespace std;
 
 class Solution {
 public:
-    void reverseString(vector<char>& s) {
-        int i = 0;
-        while(i < s.size() / 2){
-            swap(s[i], s[s.size() - i - 1]);
-            i++;
+    int minimumChairs(string s) {
+        int maxBalance = INT_MIN;
+        int balance = 0;
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s[i] == 'E') balance++;
+            else if(s[i] == 'L') balance--;
+            
+            maxBalance = max(maxBalance, balance);
         }
-        return;
+        
+        return abs(maxBalance);
     }
 };
