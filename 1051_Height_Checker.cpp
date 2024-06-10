579232570fc7a89504e195c71b1489f74d58d31d
SanskarGithub07 <117458101+SanskarGithub07@users.noreply.github.com>
2024-06-10 10:41:17 +0530

    Better solution for height checker for interviews

--- 1051_Height_Checker.cpp
+++ 1051_Height_Checker.cpp
@@ -3,13 +3,31 @@ using namespace std;
 
 class Solution {
 public:
+    void countSortHeights(vector<int> &expected){
+        vector<int> countArray(101,0);
+        for(int i = 0 ; i < expected.size() ; i++){
+            countArray[expected[i]]++;
+        }
+
+        int index = 0;
+        for(int i = 0 ; i < countArray.size() ; i++){
+            while(countArray[i] > 0){
+                expected[index] = i;
+                index++;
+                countArray[i]--;
+            }
+        }
+
+        return;
+    }
+
     int heightChecker(vector<int>& heights) {
         vector<int> expected;
         for(int i = 0 ; i < heights.size() ; i++){
             expected.push_back(heights[i]);
         }
 
-        sort(expected.begin(), expected.end());
+        countSortHeights(expected);
 
         int ans = 0;
         for(int i = 0 ; i < expected.size() ; i++){
