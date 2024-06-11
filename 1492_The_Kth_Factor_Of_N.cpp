d8f1a69b5e1fd4bab8794de14b03ba8483fc500d
SanskarGithub07 <117458101+SanskarGithub07@users.noreply.github.com>
2024-06-11 18:04:14 +0530

    Amazon problem.

---
+++ 1492_The_Kth_Factor_Of_N.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        for(int i = 1 ; i <= n ; i++){
+            if(n % i == 0){
+                k--;
+                if(k == 0){
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
