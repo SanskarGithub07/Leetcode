4a0fc3cb1f335a03c926859f038322040208863d
SanskarGithub07 <117458101+SanskarGithub07@users.noreply.github.com>
2024-06-13 10:42:25 +0530

    Greedy sorting daily challenge problem.

---
+++ 2037_Minimum_Number_Of_Moves_To_Seat_Everyone.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+
+        int ans = 0;
+        for(int i = 0 ; i < seats.size() ; i++){
+            ans += abs(students[i] - seats[i]);
+        }
+
+        return ans;
+    }
+};
