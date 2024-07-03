7c16556b8c887ad3fde8484a3b477c7ed79c2476
SanskarGithub07 <117458101+SanskarGithub07@users.noreply.github.com>
2024-07-04 01:09:50 +0530

    Leetcode 75 prefix sum ez.

---
+++ 724_Find_Pivot_Index.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        for(int i = 1 ; i < nums.size() ; i++){
+            nums[i] += nums[i - 1];
+        }
+
+        if(nums[nums.size() - 1] - nums[0] == 0){
+            return 0;
+        }
+
+        for(int i = 1 ; i < nums.size() ; i++){
+            if((nums[nums.size() - 1] - nums[i]) == nums[i - 1]){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
