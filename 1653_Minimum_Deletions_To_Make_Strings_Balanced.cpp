e2d632c9aa873ca24521450ea2f6d41e1198b801
SanskarGithub07 <117458101+SanskarGithub07@users.noreply.github.com>
2024-07-30 22:23:48 +0530

    Medium level stack + string problem daily challenge 30 july 2024.

---
+++ 1653_Minimum_Deletions_To_Make_Strings_Balanced.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minimumDeletions(string s){
+        stack<char> stk;
+        int ans = 0;
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s[i] == 'b'){
+                stk.push(s[i]);
+            }
+            else if(s[i] == 'a' && !stk.empty()){
+                stk.pop();
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
